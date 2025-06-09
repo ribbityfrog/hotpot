e1fb150c0ee0595c87e24b1b73c2f8ea5c014d4e
LeX <pierre@lastexile.fr>
2025-06-09 17:54:06 +0200

    Fixing Colors drawer flex

--- app/components/drawer/Colors.vue
+++ app/components/drawer/Colors.vue
@@ -2,7 +2,7 @@
     <UDrawer :overlay="false" :modal="false">
         <UButton label="Colors" color="info" trailing-icon="i-lucide-chevron-up" variant="subtle" />
         <template #content>
-            <Flex center class="gap-8 px-4 py-8">
+            <Flex col center class="gap-8 px-4 py-8">
                 <DarkSwitch />
                 <Flex center class="gap-6" wrap>
                     <Flex
